0d99b7b039889877e9974dce39b4fd2162b10c4f
Rajiv Shankar <rajiv.shankar.email@gmail.com>
2016-11-12 18:09:36 -0500

    revisions

--- js/helper.js
+++ js/helper.js
@@ -39,14 +39,14 @@ var HTMLprojectImage = '<img src="%data%">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
 var HTMLschoolName = '<a href="#">%data%';
-var HTMLschoolDegree = ' -- %data%</a>';
+var HTMLschoolDegree = '&nbsp; \u2014 &nbsp;%data%</a>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 
-var HTMLonlineClasses = '<h3>Certificates</h3>';              // CHANGED
+var HTMLonlineClasses = '<h3>Certificates</h3>';
 var HTMLonlineTitle = '<a href="#">%data%';
-var HTMLonlineSchool = ' - %data%</a>';
+var HTMLonlineSchool = '&nbsp; \u2014 &nbsp;%data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
 var HTMLonlineURL = '<br><a href="#">%data%</a>';
 
